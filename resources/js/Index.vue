cbd4fab4a8897bf9a29762aca9538c2107b61a54
Chia-hong Chou <chia-hongchou@Olivers-MacBook-Pro.local>
2022-02-06 09:17:40 +0800

    styling the navbar

--- resources/js/Index.vue
+++ resources/js/Index.vue
@@ -1,7 +1,17 @@
 <template>
   <div>
-    <router-link to="/">Home</router-link>
-    <router-link to="/second">Second</router-link>
-    <router-view></router-view>
+    <nav class="navbar bg-white border-bottom navbar-light">
+      <div class="container">
+        <router-link class="navbar-brand mr-auto" :to="{ name: 'home' }"
+          >LaravelBnb</router-link
+        >
+        <router-link class="btn nav-button" :to="{ name: 'second' }"
+          >Second</router-link
+        >
+      </div>
+    </nav>
+    <div class="container my-4 px-4">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
