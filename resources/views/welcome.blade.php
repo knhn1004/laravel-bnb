f7c82e9f52882adba68e3f6f5794633901c4bc87
Chia-hong Chou <chia-hongchou@Olivers-MacBook-Pro.local>
2022-02-06 08:55:56 +0800

    bootstrap, styling, and laravel mix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LaravelBnb</title>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,600">
+    <link rel="stylesheet" href="{{ asset('css/app.css')}}">
 </head>
 <body>
     <div id="app">
